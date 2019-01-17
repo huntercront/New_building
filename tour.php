5b06ffe9b7892ad7a627bf62e0fc578c03f940f7
huntercront <38252870+huntercront@users.noreply.github.com>
2019-01-17 10:04:04 +0300

    Complete all

--- tour.php
+++ tour.php
@@ -23,7 +23,7 @@ else {
 		// Сюда введите Ваш email
 		$emailTo = 'cront20097@gmail.com';
 
-		$subject = 'Заказ тура';
+		$subject = 'Заказ автобусного тура';
 		$subject = '=?utf-8?b?'. base64_encode($subject) .'?=';
 		$headers = "Content-type: text/plain; charset=\"utf-8\"\r\n";
 		$headers .= "From: ". $fromName ." <". $fromMail ."> \r\n";
